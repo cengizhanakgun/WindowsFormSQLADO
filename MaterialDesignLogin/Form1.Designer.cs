df9a1d86a7469f90898a34452b343b3074187391
cengizhanakgun <coderista@gmail.com>
2023-06-02 01:38:01 +0300

    Add project files.

---
+++ MaterialDesignLogin/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace MaterialDesignLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mUsername = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.mPassword = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.materialCheckBox1 = new MaterialSkin.Controls.MaterialCheckBox();
+            this.materialRaisedButton1 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialDivider1 = new MaterialSkin.Controls.MaterialDivider();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.SuspendLayout();
+            // 
+            // mUsername
+            // 
+            this.mUsername.Depth = 0;
+            this.mUsername.Hint = "Username";
+            this.mUsername.Location = new System.Drawing.Point(24, 83);
+            this.mUsername.MouseState = MaterialSkin.MouseState.HOVER;
+            this.mUsername.Name = "mUsername";
+            this.mUsername.PasswordChar = '\0';
+            this.mUsername.SelectedText = "";
+            this.mUsername.SelectionLength = 0;
+            this.mUsername.SelectionStart = 0;
+            this.mUsername.Size = new System.Drawing.Size(253, 23);
+            this.mUsername.TabIndex = 0;
+            this.mUsername.UseSystemPasswordChar = false;
+            // 
+            // mPassword
+            // 
+            this.mPassword.Depth = 0;
+            this.mPassword.Hint = "Password";
+            this.mPassword.Location = new System.Drawing.Point(24, 132);
+            this.mPassword.MouseState = MaterialSkin.MouseState.HOVER;
+            this.mPassword.Name = "mPassword";
+            this.mPassword.PasswordChar = '*';
+            this.mPassword.SelectedText = "";
+            this.mPassword.SelectionLength = 0;
+            this.mPassword.SelectionStart = 0;
+            this.mPassword.Size = new System.Drawing.Size(253, 23);
+            this.mPassword.TabIndex = 1;
+            this.mPassword.UseSystemPasswordChar = false;
+            // 
+            // materialCheckBox1
+            // 
+            this.materialCheckBox1.AutoSize = true;
+            this.materialCheckBox1.Depth = 0;
+            this.materialCheckBox1.Font = new System.Drawing.Font("Roboto", 10F);
+            this.materialCheckBox1.Location = new System.Drawing.Point(24, 183);
+            this.materialCheckBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.materialCheckBox1.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.materialCheckBox1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialCheckBox1.Name = "materialCheckBox1";
+            this.materialCheckBox1.Ripple = true;
+            this.materialCheckBox1.Size = new System.Drawing.Size(120, 30);
+            this.materialCheckBox1.TabIndex = 2;
+            this.materialCheckBox1.Text = "Remember me";
+            this.materialCheckBox1.UseVisualStyleBackColor = true;
+            // 
+            // materialRaisedButton1
+            // 
+            this.materialRaisedButton1.Depth = 0;
+            this.materialRaisedButton1.Location = new System.Drawing.Point(176, 245);
+            this.materialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton1.Name = "materialRaisedButton1";
+            this.materialRaisedButton1.Primary = true;
+            this.materialRaisedButton1.Size = new System.Drawing.Size(101, 30);
+            this.materialRaisedButton1.TabIndex = 3;
+            this.materialRaisedButton1.Text = "Login";
+            this.materialRaisedButton1.UseVisualStyleBackColor = true;
+            this.materialRaisedButton1.Click += new System.EventHandler(this.materialRaisedButton1_Click);
+            // 
+            // materialDivider1
+            // 
+            this.materialDivider1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialDivider1.Depth = 0;
+            this.materialDivider1.Location = new System.Drawing.Point(-2, 317);
+            this.materialDivider1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialDivider1.Name = "materialDivider1";
+            this.materialDivider1.Size = new System.Drawing.Size(303, 1);
+            this.materialDivider1.TabIndex = 3;
+            this.materialDivider1.Text = "materialDivider1";
+            this.materialDivider1.Click += new System.EventHandler(this.materialDivider1_Click);
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(20, 332);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(148, 19);
+            this.materialLabel1.TabIndex = 4;
+            this.materialLabel1.Text = "Lost your password?";
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel2.Location = new System.Drawing.Point(213, 332);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(64, 19);
+            this.materialLabel2.TabIndex = 5;
+            this.materialLabel2.Text = "Register";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 394);
+            this.Controls.Add(this.materialLabel2);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.materialDivider1);
+            this.Controls.Add(this.materialRaisedButton1);
+            this.Controls.Add(this.materialCheckBox1);
+            this.Controls.Add(this.mPassword);
+            this.Controls.Add(this.mUsername);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialSingleLineTextField mUsername;
+        private MaterialSkin.Controls.MaterialSingleLineTextField mPassword;
+        private MaterialSkin.Controls.MaterialCheckBox materialCheckBox1;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton1;
+        private MaterialSkin.Controls.MaterialDivider materialDivider1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+    }
+}
+
---
+++ MaterialDesignLogin/Form1.cs
@@ -0,0 +1,75 @@
+﻿using MaterialSkin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace MaterialDesignLogin
+{
+    public partial class Form1 : MaterialSkin.Controls.MaterialForm
+    {
+        SqlConnection connection = new SqlConnection("Data Source=DESKTOP-P7OC757\\SQLEXPRESS; Initial Catalog=App1; Integrated Security=TRUE");
+        public Form1()
+        {
+            InitializeComponent();
+            MaterialSkin.MaterialSkinManager skinManager = MaterialSkin.MaterialSkinManager.Instance;
+            skinManager.AddFormToManage(this);
+            skinManager.Theme = MaterialSkin.MaterialSkinManager.Themes.DARK;
+
+            skinManager.ColorScheme = new MaterialSkin.ColorScheme(MaterialSkin.Primary.Green600, MaterialSkin.Primary.BlueGrey900, MaterialSkin.Primary.BlueGrey500, MaterialSkin.Accent.Orange700, MaterialSkin.TextShade.WHITE);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+
+        }
+
+        private void materialDivider1_Click(object sender, EventArgs e)
+        {
+
+        }
+        bool isThere;
+        private void materialRaisedButton1_Click(object sender, EventArgs e)
+        {
+            string userName = mUsername.Text;
+            string password = mPassword.Text;
+           
+
+            connection.Open();
+            SqlCommand command = new SqlCommand("Select *from login", connection);
+            SqlDataReader reader = command.ExecuteReader(); 
+
+            while(reader.Read())
+            {
+                if (userName == reader["userName"].ToString().TrimEnd() && password == reader["password"].ToString().TrimEnd())
+                {
+                    isThere = true; 
+                    break;
+                }
+                else
+                {
+                    isThere = false;    
+
+                }
+                connection.Close();
+
+            }
+            if (isThere)
+            {
+                MessageBox.Show("Başarılı bir şekilde giriş yaptınız");
+            }
+            else
+            {
+                MessageBox.Show("Giriş başarısız !!!");
+            }
+        }
+    }
+}
